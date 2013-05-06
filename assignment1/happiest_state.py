058c6947d2a640f640a5e7a2b6d30d321e236f8c
beardygnome <github@beardygnome.co.uk>
2013-05-06 20:49:58 +0100

    first attempt at problem 5

--- assignment1/happiest_state.py
+++ assignment1/happiest_state.py
@@ -4,6 +4,33 @@
 import json
 import sys
 
+def get_tweet_suitability(tweet):
+    """(dict(str, str)) -> (bool, str)
+
+    Return True and the state code if the tweet is suitable for analysis,
+    otherwise False and the state code, if appropriate.
+    """
+
+    result = False
+    state = ""
+
+    if not "delete" in tweet:
+        place = tweet["place"]
+
+        if not place == None:
+            country_code = place["country_code"]
+
+            if country_code == "US":
+                full_name = place["full_name"]
+                city, state = full_name.split(",")
+                state = state.lstrip()
+
+                if not (state == "US" or len(state) > 2):
+                    result = True
+
+    return result, state
+
+
 def main():
     """(NoneType) -> NoneType
 
@@ -16,16 +43,34 @@ def main():
     sentiments = process_sentiments(sent_file)
 
     i = 0
+    states = {}
+
     for line in tweet_file:
-        if i < 500:
-            i += 1
-        else:
-            break
+        #if i < 5000:
+        #    i += 1
+        #else:
+        #    break
 
-        score = process_tweet(line, sentiments)
+        tweet = json.loads(line)
+        tweet_suitable, state = get_tweet_suitability(tweet)
+        sentiment = 0.0
 
-        print score
+        if tweet_suitable:
+            sentiment = process_tweet(tweet, sentiments)
 
+            if state in states:
+                states[state] += sentiment
+            else:
+                states[state] = sentiment
+
+    state_list = []
+
+    for state in states:
+        state_list.append([states[state], state])
+
+    state_list.sort(reverse=True)
+
+    print state_list[0][1].encode("utf-8")
 
 def process_sentiments(scores):
     """(File) -> dict(str, float)
@@ -44,26 +89,21 @@ def process_sentiments(scores):
 
 
 def process_tweet(tweet, scores):
-    """(File, dict(str, float)) -> (str, float)
+    """(dict(str, str), dict(str, float)) -> float
 
     Return the state and the overall score of each tweet.
     """
 
     place = ""
     sentiment = 0.0
-    tweet = json.loads(tweet)
 
-    if not "delete" in tweet:
-        if not tweet["place"] == None:
-            place = tweet["place"]["code"]
-            tweet_text = tweet["text"]
+    tweet_text = tweet["text"]
 
-            for word in tweet_text.split():
-                if word in scores:
-                    sentiment += scores[word]
+    for word in tweet_text.split():
+        if word in scores:
+            sentiment += scores[word]
 
-
-    return place, sentiment
+    return sentiment
 
 
 if __name__ == '__main__':
