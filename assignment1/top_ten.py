36f2adad1cf0bca671f56be706d021a531d885cc
beardygnome <github@beardygnome.co.uk>
2013-05-06 20:59:41 +0100

    started problem 6

---
+++ assignment1/top_ten.py
@@ -0,0 +1,15 @@
+#!/usr/bin/ev python
+# -*- coding: utf-8 -*-
+
+import json
+import sys
+
+def main():
+	"""(NoneType) -> NoneType
+
+	Run the program.
+	"""
+
+
+if __name__ == "__main__":
+	main()
