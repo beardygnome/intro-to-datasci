58cd8a8621d8c1c7aa43d8c01c104930eea315d4
beardygnome <github@beardygnome.co.uk>
2013-05-06 18:35:51 +0100

    updated for submission

--- assignment1/frequency.py
+++ assignment1/frequency.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import json
 import sys
 
 def main():
@@ -12,13 +13,40 @@ def main():
     tweet_texts = []
 
     for line in tweet_file:
-        tweet_text == process_tweet(line)
+        tweet_text = process_tweet(line)
 
         if tweet_text:
-            tweet_texts.add(tweet_text)
+            tweet_texts.append(tweet_text)
+
+    #print tweet_texts
+
+    words = {}
+    tot_num_words = 0.0
+
+    for text in tweet_texts:
+        for word in text.split():
+            tot_num_words += 1.0
+
+            if word in words:
+                words[word] += 1.0
+            else:
+                words[word] = 1.0
+
+    for word in words:
+        words[word] = words[word] / tot_num_words
+
+    i = 0
+    for word in words:
+        #if i < 20:
+        #    i += 1
+        #else:
+        #    break
+
+        print word, words[word]
 
 
-def process_tweets(tweet):
+
+def process_tweet(tweet):
     """(str) -> str
 
     Return the tweet text for the tweet
