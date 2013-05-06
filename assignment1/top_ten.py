a28eac87381ac950dc7aed6f8cb77701aaad9b89
beardygnome <github@beardygnome.co.uk>
2013-05-06 21:53:05 +0100

    first attempt at problem 6

--- assignment1/top_ten.py
+++ assignment1/top_ten.py
@@ -5,49 +5,49 @@ import json
 import sys
 
 def main():
-	"""(NoneType) -> NoneType
+    """(NoneType) -> NoneType
 
-	Run the program.
-	"""
+    Run the program.
+    """
 
-	tweet_file = open(sys.argv[1])
+    tweet_file = open(sys.argv[1])
 
-	i = 0
+    i = 0
 
-	hashtag_dict = {}
+    hashtag_dict = {}
 
-	for line in tweet_file:
-		tweet = json.loads(line)
+    for line in tweet_file:
+        tweet = json.loads(line)
 
-		if "entities" in tweet:
-			entities = tweet["entities"]
+        if "entities" in tweet:
+            entities = tweet["entities"]
 
-			if "hashtags" in entities and len(entities["hashtags"]) > 0:
-				hashtags = entities["hashtags"]
+            if "hashtags" in entities and len(entities["hashtags"]) > 0:
+                hashtags = entities["hashtags"]
 
-				for hashtag in hashtags:
-					hashtag_text = hashtag["text"]
+                for hashtag in hashtags:
+                    hashtag_text = hashtag["text"]
 
-					if hashtag_text in hashtag_dict:
-						hashtag_dict[hashtag_text] += 1.0
-					else:
-						hashtag_dict[hashtag_text] = 1.0
+                    if hashtag_text in hashtag_dict:
+                        hashtag_dict[hashtag_text] += 1.0
+                    else:
+                        hashtag_dict[hashtag_text] = 1.0
 
-				if i < 200:
-					i += 1
-				else:
-					break
+                #if i < 200:
+                #   i += 1
+                #else:
+                #   break
 
-	hashtag_list = []
+    hashtag_list = []
 
-	for hashtag in hashtag_dict:
-		hashtag_list.append([hashtag_dict[hashtag], hashtag])
+    for hashtag in hashtag_dict:
+        hashtag_list.append([hashtag_dict[hashtag], hashtag])
 
-	hashtag_list.sort(reverse=True)
+    hashtag_list.sort(reverse=True)
 
-	for i in range(10):
-		print hashtag_list[i][1], hashtag_list[i][0]
+    for i in range(10):
+        print hashtag_list[i][1], hashtag_list[i][0]
 
 
 if __name__ == "__main__":
-	main()
+    main()
