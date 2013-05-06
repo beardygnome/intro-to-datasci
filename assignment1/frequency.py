71dbfb9aa5e84a34cc1262c6c27337e9861601b1
beardygnome <github@beardygnome.co.uk>
2013-05-06 18:22:07 +0100

    updated

--- assignment1/frequency.py
+++ assignment1/frequency.py
@@ -1,2 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+import sys
+
+def main():
+    """(NoneType) -> NoneType
+
+    Run the program
+    """
+    tweet_file = open(sys.argv[1])
+    tweet_texts = []
+
+    for line in tweet_file:
+        tweet_text == process_tweet(line)
+
+        if tweet_text:
+            tweet_texts.add(tweet_text)
+
+
+def process_tweets(tweet):
+    """(str) -> str
+
+    Return the tweet text for the tweet
+    """
+
+    tweet = json.loads(tweet)
+    tweet_text = None
+
+    if not "delete" in tweet:
+        tweet_text = tweet["text"]
+
+    return tweet_text
+
+
+if __name__ == "__main__":
+    main()
