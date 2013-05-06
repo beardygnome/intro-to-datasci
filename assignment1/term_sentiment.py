bfad19fd8da8753f3a001cc8664f9e7edb382781
beardygnome <github@beardygnome.co.uk>
2013-05-06 16:51:49 +0100

    updated

--- assignment1/term_sentiment.py
+++ assignment1/term_sentiment.py
@@ -7,11 +7,17 @@ def lines(fp):
     print str(len(fp.readlines()))
 
 def main():
+    """(NoneType) -> NoneType
+
+    Run the program
+    """
+
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    #hw()
+    #lines(sent_file)
+    #lines(tweet_file)
+
 
 if __name__ == '__main__':
     main()
