1794e9bf3cde101bee3f6d56d6be7fdf875fe628
beardygnome <github@beardygnome.co.uk>
2013-05-27 20:38:28 +0100

    updated comment in problem 5 script

--- assignment3/unique_trims.py
+++ assignment3/unique_trims.py
@@ -2,7 +2,7 @@ import MapReduce
 import sys
 
 """
-Social Network Friend Count Example in the Simple Python MapReduce Framework
+DNA Nucleotide Trimmimg Example in the Simple Python MapReduce Framework
 """
 
 mr = MapReduce.MapReduce()
