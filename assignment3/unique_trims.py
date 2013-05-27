ad9208caaaaec987da2c89b93f73c45e53565ea7
beardygnome <github@beardygnome.co.uk>
2013-05-27 20:33:16 +0100

    completed problem 5

---
+++ assignment3/unique_trims.py
@@ -0,0 +1,33 @@
+import MapReduce
+import sys
+
+"""
+Social Network Friend Count Example in the Simple Python MapReduce Framework
+"""
+
+mr = MapReduce.MapReduce()
+
+# =============================
+# Do not modify above this line
+
+def mapper(record):
+    # seq_id: the sequence identifier
+    # nucleotides: the sequence of nucleotides
+
+    seq_id = record[0]
+    nucleotides = record[1]
+
+    mr.emit_intermediate(nucleotides[:-10], 1)
+
+
+def reducer(key, occurrence_list):
+    # key: trimmed nucleotide string
+    # ocurrence_list: list of ocuurence counts
+
+    mr.emit(key)
+
+# Do not modify below this line
+# =============================
+if __name__ == '__main__':
+    inputdata = open(sys.argv[1])
+    mr.execute(inputdata, mapper, reducer)
