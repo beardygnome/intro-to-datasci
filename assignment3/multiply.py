1516fb7c67dc09e47f1265303c360e784127cd27
beardygnome <github@beardygnome.co.uk>
2013-05-27 21:45:46 +0100

    updated comment in problem 6 script

--- assignment3/multiply.py
+++ assignment3/multiply.py
@@ -2,7 +2,7 @@ import MapReduce
 import sys
 
 """
-Social Network Friend Count Example in the Simple Python MapReduce Framework
+Sparse Matrix Multiplication Example in the Simple Python MapReduce Framework
 """
 
 mr = MapReduce.MapReduce()
